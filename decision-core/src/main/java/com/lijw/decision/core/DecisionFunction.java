a8fa164860b72445c9215f42703d621655abd4d9
xcupoem <takealeaf@yeah.net>
2022-01-08 23:21:57 +0800

    决策项名称

--- decision-core/src/main/java/com/lijw/decision/core/DecisionFunction.java
+++ decision-core/src/main/java/com/lijw/decision/core/DecisionFunction.java
@@ -2,8 +2,12 @@ package com.lijw.decision.core;
 
 public interface DecisionFunction extends Decidable, Decision {
 
-	default <T> String getDecisionName(Class<T> clazz) {
-		return clazz.getClass().getSimpleName();
+	/**
+	 * 决策名称，表示当前的决策数据所用是哪个决策，实现类可按需定义，对于主流程不是必须的
+	 * @return
+	 */
+	default String getDecisionName() {
+		return null;
 	}
 
 }
