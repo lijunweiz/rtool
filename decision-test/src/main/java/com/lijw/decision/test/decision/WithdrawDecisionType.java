ed58904c89a434e2aa9cd0de9dea3d5d2c7e52ee
lijunweiz <takealeaf@yeah.net>
2022-02-03 15:19:47 +0800

    test

--- decision-test/src/main/java/com/lijw/decision/test/decision/WithdrawDecisionType.java
+++ decision-test/src/main/java/com/lijw/decision/test/decision/WithdrawDecisionType.java
@@ -1,12 +1,13 @@
 package com.lijw.decision.test.decision;
 
 import com.lijw.decision.core.AbstractDecisionType;
+import com.lijw.decision.core.Context;
 import com.lijw.decision.core.DefaultValue;
 
 public class WithdrawDecisionType extends AbstractDecisionType {
 
     @Override
-    public boolean process() {
+    public boolean process(Context context) {
         return DefaultValue.TRUE.booleanValue();
     }
 
