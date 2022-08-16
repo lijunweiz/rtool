9e38b41b96f1c621d489054b72b15f2e41718988
lijunweiz <takealeaf@yeah.net>
2022-08-16 21:20:49 +0800

    使用样例springboot调整

--- rtool-boot/src/test/java/com/test/App.java
+++ rtool-boot/src/test/java/com/test/App.java
@@ -2,46 +2,30 @@ package com.test;
 
 import com.lijunweiz.rtool.core.CompositeRule;
 import com.lijunweiz.rtool.core.Context;
-import com.lijunweiz.rtool.core.Rule;
 import com.lijunweiz.rtool.core.RuleTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@RestController
 @SpringBootApplication
 public class App {
 
-    private Logger logger = LoggerFactory.getLogger(App.class);
-
-    @Resource
-    private ApplicationContext applicationContext;
-
-    @Resource
-    private Rule compositeRule;
-
-    @Resource
-    private RuleTemplate ruleTemplate;
+    private static Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
-    }
+        ApplicationContext context = SpringApplication.run(App.class, args);
+        RuleTemplate ruleTemplate = context.getBean(RuleTemplate.class);
+        CompositeRule compositeRule = context.getBean(CompositeRule.class);
 
-    @RequestMapping("/g")
-    public void test01() {
         logger.info("对比ruleTemplate.getRule() == compositeRule = {}", ruleTemplate.getRule() == compositeRule);
         List<String> collect = CompositeRule.class.cast(ruleTemplate.getRule()).getRules().stream().map(x -> x.getName()).collect(Collectors.toList());
-        logger.info("规则: " + collect.toString());
+        logger.info("规则: " + collect);
         ruleTemplate.execute(new Context());
     }
-
 
 }
